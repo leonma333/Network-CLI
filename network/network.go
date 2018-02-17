c1045fab41c89edefbceb9b66187f8bb7c7a26fb
Liang-Hsuan Ma <leon.ma@shopify.com>
2018-02-17 13:37:46 -0500

    Refactor code to make network its own package

--- network/network.go
+++ network/network.go
@@ -1,187 +1,193 @@
-package main
+package network
 
 import (
-  "fmt"
-  "net"
-  "net/http"
-  "strconv"
-  "io"
-  "io/ioutil"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"strconv"
+)
+
+const (
+	DEFAULT_PORT = 8080
+	MAX_PORT     = 65535
 )
 
 // Network interface to deal with http server and IP
 type Network interface {
-  startHttpServer(port int, useFile bool) error
-  getAllUnavailablePorts() portList
-  getAllUnavailablePortsFromList(pl *portList) portList
-  portIsAvailable(port int) (status bool, err error)
-  getInternalIp() (net.IP, error)
-  getExternalIp() (net.IP, error)
-  forwarding(target string, port int) error
-  forward(conn net.Conn, target string)
+	StartHttpServer(port int, useFile bool) error
+	AllUnavailablePorts() PortList
+	AllUnavailablePortsFromList(pl *PortList) PortList
+	PortIsAvailable(port int) (status bool, err error)
+	InternalIP() (net.IP, error)
+	ExternalIP() (net.IP, error)
+	Forwarding(target string, port int) error
+	forward(conn net.Conn, target string)
 }
 
 // NetworkHelper implements Network interface
-type NetworkHelper struct {}
+type networkHandler struct{}
 
-const DEFUALT_PORT = 8080
-const MAX_PORT = 65535
+func NewNetwork() Network {
+	return &networkHandler{}
+}
 
 /*
  * Start a local http server with the port number provided
  */
-func (network NetworkHelper) startHttpServer(port int, useFile bool) error {
-  if useFile {
-    http.Handle("/", http.FileServer(http.Dir("./")))
-  } else {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-      fmt.Fprintf(w, "You're now on port %v [%s]", port, r.URL.Path[0:])
-    })
-  }
+func (network *networkHandler) StartHttpServer(port int, useFile bool) error {
+	if useFile {
+		http.Handle("/", http.FileServer(http.Dir("./")))
+	} else {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "You're now on port %v [%s]", port, r.URL.Path[0:])
+		})
+	}
 
-  //go func() {
-    if err := http.ListenAndServe(portStringify(port), nil); err != nil {
-      return err
-    }
-  //}()
-  return nil
+	//go func() {
+	if err := http.ListenAndServe(portStringify(port), nil); err != nil {
+		return err
+	}
+	//}()
+	return nil
 }
 
 /*
  * Return all the unavailable port number from the machine
  */
-func (network NetworkHelper) getAllUnavailablePorts() portList {
-  var unavailablePorts portList
-  for i := 0; i <= MAX_PORT; i++ {
-    if status, _ := network.portIsAvailable(i); !status {
-      unavailablePorts = append(unavailablePorts, i)
-    }
-  }
-  return unavailablePorts
+func (network *networkHandler) AllUnavailablePorts() PortList {
+	var unavailablePorts PortList
+	for i := 0; i <= MAX_PORT; i++ {
+		if status, _ := network.PortIsAvailable(i); !status {
+			unavailablePorts = append(unavailablePorts, i)
+		}
+	}
+	return unavailablePorts
 }
 
 /*
  * Return all the unavailable port number from the given list of port numbers
  */
-func (network NetworkHelper) getAllUnavailablePortsFromList(pl *portList) portList {
-  var unavailablePorts portList
-  for _, port := range *pl {
-    if status, _ := network.portIsAvailable(port); !status {
-      unavailablePorts = append(unavailablePorts, port)
-    }
-  }
-  return unavailablePorts
+func (network *networkHandler) AllUnavailablePortsFromList(pl *PortList) PortList {
+	var unavailablePorts PortList
+	for _, port := range *pl {
+		if status, _ := network.PortIsAvailable(port); !status {
+			unavailablePorts = append(unavailablePorts, port)
+		}
+	}
+	return unavailablePorts
 }
 
 /*
  * Check the given port number is availble to be used for the machine
  */
-func (network NetworkHelper) portIsAvailable(port int) (status bool, err error) {
-  host := ":" + strconv.Itoa(port)
-  server, err := net.Listen("tcp", host)
-  if err != nil {
-    return false, err
-  }
-  server.Close()
-  return true, nil
+func (network *networkHandler) PortIsAvailable(port int) (status bool, err error) {
+	host := ":" + strconv.Itoa(port)
+	server, err := net.Listen("tcp", host)
+	if err != nil {
+		return false, err
+	}
+	server.Close()
+	return true, nil
 }
 
 /*
  * Retrieve machine local IP address
  */
-func (network NetworkHelper) getInternalIp() (net.IP, error) {
-  // Dial to connect to local server
-  conn, err := net.Dial("udp", "8.8.8.8:80")
+func (network *networkHandler) InternalIP() (net.IP, error) {
+	// Dial to connect to local server
+	conn, err := net.Dial("udp", "8.8.8.8:80")
 
-  if err != nil {
-    return nil, err
-  }
+	if err != nil {
+		return nil, err
+	}
 
-  // Close the connection when the response is read
-  defer conn.Close()
+	// Close the connection when the response is read
+	defer conn.Close()
 
-  // Get the local IP address
-  localAddr := conn.LocalAddr().(*net.UDPAddr)
+	// Get the local IP address
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
 
-  return localAddr.IP, nil
+	return localAddr.IP, nil
 }
 
 /*
  * Retrieve machine external IP address
  */
-func (network NetworkHelper) getExternalIp() (net.IP, error) {
-  // Get request on myexternalip.com to retrieve external IP address for the machine
-  resp, err := http.Get("http://myexternalip.com/raw")
+func (network *networkHandler) ExternalIP() (net.IP, error) {
+	// Get request on myexternalip.com to retrieve external IP address for the machine
+	resp, err := http.Get("http://myexternalip.com/raw")
 
-  if err != nil {
-    return nil, err
-  }
+	if err != nil {
+		return nil, err
+	}
 
-  // Close the connection when response body is read
-  defer resp.Body.Close()
+	// Close the connection when response body is read
+	defer resp.Body.Close()
 
-  // Read response body
-  body, err := ioutil.ReadAll(resp.Body)
+	// Read response body
+	body, err := ioutil.ReadAll(resp.Body)
 
-  if err != nil {
-    return nil, err
-  }
+	if err != nil {
+		return nil, err
+	}
 
-  ipStr := string(body)
+	ipStr := string(body)
 
-  // Convert body byte array to string and return it
-  return net.ParseIP(ipStr[:len(ipStr)-1]), nil
+	// Convert body byte array to string and return it
+	return net.ParseIP(ipStr[:len(ipStr)-1]), nil
 }
 
 /*
  * Port forwarding
  */
-func (network NetworkHelper) forwarding(target string, port int) error {
-  // Declare listener to the origin port
-  listener, err := net.Listen("tcp", portStringify(port))
-  if err != nil {
-    return err
-  }
+func (network *networkHandler) Forwarding(target string, port int) error {
+	// Declare listener to the origin port
+	listener, err := net.Listen("tcp", portStringify(port))
+	if err != nil {
+		return err
+	}
 
-  for {
-    // Start connection to listener
-    conn, err := listener.Accept()
-    if err != nil {
-      return err
-    }
-    fmt.Printf("Accepted connection %v\n", conn)
-    go network.forward(conn, target)
-  }
+	for {
+		// Start connection to listener
+		conn, err := listener.Accept()
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Accepted connection %v\n", conn)
+		go network.forward(conn, target)
+	}
 }
 
 /*
  * Forward connection to then given port number
  */
-func (network NetworkHelper) forward(conn net.Conn, target string) {
-  // Declare client to the forwarding port
-  client, err := net.Dial("tcp", target)
-  if err != nil {
-    panic(err)
-  }
+func (network *networkHandler) forward(conn net.Conn, target string) {
+	// Declare client to the forwarding port
+	client, err := net.Dial("tcp", target)
+	if err != nil {
+		panic(err)
+	}
 
-  fmt.Printf("Connected to localhost %v\n", conn)
+	fmt.Printf("Connected to localhost %v\n", conn)
 
-  // Copy IO
-  go func() {
-    defer client.Close()
-    defer conn.Close()
-    io.Copy(client, conn)
-  }()
-  go func() {
-    defer client.Close()
-    defer conn.Close()
-    io.Copy(conn, client)
-  }()
+	// Copy IO
+	go func() {
+		defer client.Close()
+		defer conn.Close()
+		io.Copy(client, conn)
+	}()
+	go func() {
+		defer client.Close()
+		defer conn.Close()
+		io.Copy(conn, client)
+	}()
 }
 
 /*
  * Make port number to string format ":xxx"
  */
 func portStringify(port int) string {
-  return fmt.Sprintf(":%v", port)
+	return fmt.Sprintf(":%v", port)
 }
--- network/portList.go
+++ network/portList.go
@@ -1,33 +1,33 @@
-package main
+package network
 
 import (
-  "fmt"
-  "strings"
-  "strconv"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 // Create a new type for a list of port number
-type portList []int
+type PortList []int
 
 /*
- * Convert from portList to string implementation
+ * Convert from PortList to string implementation
  */
-func(pl *portList) String() string {
-  return fmt.Sprintf("%v", *pl)
+func (pl *PortList) String() string {
+	return fmt.Sprintf("%v", *pl)
 }
 
 /*
- * Convert from string to portList implementation
+ * Convert from string to PortList implementation
  */
-func (pl *portList) Set(value string) error {
-  convertStringArrayToPortlist := func(strArr []string) portList {
-    var list portList
-    for _, str := range strArr {
-      port, _ := strconv.Atoi(str)
-      list = append(list, port)
-    }
-    return list
-  }
-  *pl = convertStringArrayToPortlist(strings.Split(value, ","))
-  return nil
+func (pl *PortList) Set(value string) error {
+	convertStringArrayToPortlist := func(strArr []string) PortList {
+		var list PortList
+		for _, str := range strArr {
+			port, _ := strconv.Atoi(str)
+			list = append(list, port)
+		}
+		return list
+	}
+	*pl = convertStringArrayToPortlist(strings.Split(value, ","))
+	return nil
 }
